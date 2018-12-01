c2170a36ba42d4dde8df827819193297b1b59bff
Simon Boisset <simon@MacBook-Air-de-Simon.local>
2018-12-01 19:29:22 +0100

    player & weapon
    player & weapon

---
+++ Assets/Scripts/Player/bulletControler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletControler : MonoBehaviour {
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        moveDirection = new Vector3(1,0,0) * Time.deltaTime*5;
+        transform.Translate(moveDirection);
+    }
+}
