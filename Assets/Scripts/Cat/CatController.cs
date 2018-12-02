2d5c32dd2d2c479bd6884755b63715d4b773bcf5
Simon Boisset <simon@MacBook-Air-de-Simon.local>
2018-12-02 01:06:56 +0100

    Cat & Player Body

---
+++ Assets/Scripts/Cat/CatController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+    public bool follow = false;
+    public float speed = 1;
+    void Update()
+    { 
+        if (this.follow == true)
+        {
+            GameObject Player = GameObject.Find("player");
+            Vector3 moveDirection = new Vector3(Player.transform.position.x-transform.position.x, Player.transform.position.y-transform.position.y, transform.position.z);
+            moveDirection = moveDirection * speed * Time.deltaTime;
+            transform.Translate(moveDirection);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.follow |= collision.gameObject.tag == "Player";
+        if (collision.gameObject.tag == "Ennemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
