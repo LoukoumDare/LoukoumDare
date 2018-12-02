a16e1c6047c3c1cb27e2a48538c54cdc5f767b31
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2018-12-02 12:29:34 +0100

    Cat animation

--- Assets/Scripts/Cat/CatController.cs
+++ Assets/Scripts/Cat/CatController.cs
@@ -6,6 +6,9 @@ public class CatController : MonoBehaviour {
     public bool follow = false;
     public float speed = 1;
     public int health;
+
+    bool facingRight = false;
+
     void Start()
     {
         this.health = 3;
@@ -18,6 +21,12 @@ public class CatController : MonoBehaviour {
             Vector3 moveDirection = new Vector3(Player.transform.position.x-transform.position.x, Player.transform.position.y-transform.position.y, transform.position.z);
             moveDirection = moveDirection * speed * Time.deltaTime;
             transform.Translate(moveDirection);
+
+            if ((facingRight && moveDirection.x < -0.01) || (!facingRight && moveDirection.x > 0.01))
+            {
+                facingRight = !facingRight;
+                transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+            }
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
