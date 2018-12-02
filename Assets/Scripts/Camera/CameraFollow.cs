dc517b44524fd2a9913d7910cdfedbde24ad712d
Romain GROSSE <grosseromain@hotmail.fr>
2018-12-02 08:55:40 +0100

    Camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform _player;
+	Vector2 minPos;
+	Vector2 maxPos;
+
+	public BoxCollider2D _boxCollider;
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		//_boxCollider = GetComponent<BoxCollider2D>();
+		_camera = GetComponent<Camera>();
+
+		minPos = -_boxCollider.bounds.extents + new Vector3(_camera.orthographicSize * _camera.aspect, _camera.orthographicSize, 0);
+		maxPos = _boxCollider.bounds.extents - new Vector3(_camera.orthographicSize * _camera.aspect, _camera.orthographicSize, 0);
+
+		Debug.Log(minPos);
+		Debug.Log(_camera.orthographicSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Mathf.Clamp(_player.position.x,minPos.x, maxPos.x), Mathf.Clamp(_player.position.y, minPos.y, maxPos.y) , transform.position.z);
+		
+	}
+}
